ccfd6abc3bc14486b5552e37247d2cc82e9705b6
Andrey Konopkov <andrey@knpkv.dev>
2025-11-16 13:17:57 +0100

    Refactor greet function formatting

--- packages/hello/src/index.ts
+++ packages/hello/src/index.ts
@@ -1,7 +1,6 @@
 import * as Effect from "effect/Effect"
 
-export const greet = (name: string): Effect.Effect<string> =>
-  Effect.sync(() => `Hello, ${name}!`)
+export const greet = (name: string): Effect.Effect<string> => Effect.sync(() => `Hello, ${name}!`)
 
 export const greetWithPrefix = (
   prefix: string,
