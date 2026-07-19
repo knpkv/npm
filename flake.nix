4e611f78247d3aeb5e6aca303f6c44f1261b8eb4
Andrey Konopkov <andrey@knpkv.dev>
2026-07-19 11:14:33 +0200

    fix(nix): run pnpm with Node 24 (#222)

--- flake.nix
+++ flake.nix
@@ -7,15 +7,42 @@
       nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
         system: function nixpkgs.legacyPackages.${system}
       );
+    nodeToolchain = pkgs: rec {
+      nodejs = pkgs.nodejs_24;
+      pnpm = pkgs.writeShellApplication {
+        name = "pnpm";
+        text = ''
+          exec ${nodejs}/bin/corepack pnpm "$@"
+        '';
+      };
+      pnpx = pkgs.writeShellApplication {
+        name = "pnpx";
+        text = ''
+          exec ${nodejs}/bin/corepack pnpx "$@"
+        '';
+      };
+    };
   in {
     formatter = forAllSystems (pkgs: pkgs.alejandra);
-    devShells = forAllSystems (pkgs: {
+    checks = forAllSystems (pkgs: let
+      inherit (nodeToolchain pkgs) nodejs pnpm;
+    in {
+      node-pnpm-toolchain = pkgs.runCommand "node-pnpm-toolchain" {} ''
+        test "$(${nodejs}/bin/node --eval 'process.stdout.write(process.versions.node.split(".")[0])')" = 24
+        grep --fixed-strings '${nodejs}/bin/corepack pnpm' ${pnpm}/bin/pnpm
+        touch $out
+      '';
+    });
+    devShells = forAllSystems (pkgs: let
+      inherit (nodeToolchain pkgs) nodejs pnpm pnpx;
+    in {
       default = pkgs.mkShell {
         packages = with pkgs; [
           bun
           deno
-          corepack
-          nodejs_24
+          nodejs
+          pnpm
+          pnpx
           python3
         ];
       };
